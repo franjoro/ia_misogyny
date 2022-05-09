48f2815a8bd5e58ea347a9137e81c44bcc4b33e2
Franklin López <franklin.lopez@alumnos.uneatlantico.es>
2022-05-09 16:32:11 +0200

    datos

--- app.py
+++ app.py
@@ -9,7 +9,6 @@ from ai.main import ai
 app = Flask(__name__, static_url_path='/static')
 
 
-
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -17,71 +16,71 @@ def index():
 
 @app.route('/checkMessage', methods=['POST'])
 def checkMessage():
-  if request.method == 'POST':
-    textos = request.form.getlist("textos[]")
-    formatData = []
-    for i in range(len(textos)):
-        formatData.append([i, textos[i]])
+    if request.method == 'POST':
+        textos = request.form.getlist("textos[]")
+        formatData = []
+        for i in range(len(textos)):
+            formatData.append([i, textos[i]])
 
-    train = pd.DataFrame(formatData, columns=['id', 'text'])
-    train.to_csv('./data/test2.csv', index=False)
-    ai("label")
-    ai("category1")
-    ai("category2")
-    ai("category3")
-    ai("category4")
-    ai("single")
-    ai("groups")
+        train = pd.DataFrame(formatData, columns=['id', 'text'])
+        train.to_csv('./data/test2.csv', index=False)
+        ai("label")
+        ai("category1")
+        ai("category2")
+        ai("category3")
+        ai("category4")
+        ai("single")
+        ai("groups")
 
-    label = pd.read_csv('submissions/lr_submission_label.csv')
-    category1 = pd.read_csv('submissions/lr_submission_category1.csv')
-    category2 = pd.read_csv('submissions/lr_submission_category2.csv')
-    category3 = pd.read_csv('submissions/lr_submission_category3.csv')
-    category4 = pd.read_csv('submissions/lr_submission_category4.csv')
-    single = pd.read_csv('submissions/lr_submission_single.csv')
-    groups = pd.read_csv('submissions/lr_submission_groups.csv')
+        label = pd.read_csv('submissions/lr_submission_label.csv')
+        category1 = pd.read_csv('submissions/lr_submission_category1.csv')
+        category2 = pd.read_csv('submissions/lr_submission_category2.csv')
+        category3 = pd.read_csv('submissions/lr_submission_category3.csv')
+        category4 = pd.read_csv('submissions/lr_submission_category4.csv')
+        single = pd.read_csv('submissions/lr_submission_single.csv')
+        groups = pd.read_csv('submissions/lr_submission_groups.csv')
 
-    labelResults = []
-    category1Results = []
-    category2Results = []
-    category3Results = []
-    category4Results = []
-    singleResults = []
-    groupsResults = []
+        labelResults = []
+        category1Results = []
+        category2Results = []
+        category3Results = []
+        category4Results = []
+        singleResults = []
+        groupsResults = []
 
-    for index, row in label.iterrows():
-      labelResults.append(str(row['label']))
-    labelResults = ",".join(labelResults)
+        for index, row in label.iterrows():
+            labelResults.append(str(row['label']))
+        labelResults = ",".join(labelResults)
 
-    for index, row in category1.iterrows():
-      category1Results.append(str(row['category1']))
-    category1Results = ",".join(category1Results)
+        for index, row in category1.iterrows():
+            category1Results.append(str(row['category1']))
+        category1Results = ",".join(category1Results)
 
-    for index, row in category2.iterrows():
-      category2Results.append(str(row['category2']))
-    category2Results = ",".join(category2Results)
+        for index, row in category2.iterrows():
+            category2Results.append(str(row['category2']))
+        category2Results = ",".join(category2Results)
 
-    for index, row in category3.iterrows():
-      category3Results.append(str(row['category3']))
-    category3Results = ",".join(category3Results)
+        for index, row in category3.iterrows():
+            category3Results.append(str(row['category3']))
+        category3Results = ",".join(category3Results)
 
-    for index, row in category4.iterrows():
-      category4Results.append(str(row['category4']))
-    category4Results = ",".join(category4Results)
+        for index, row in category4.iterrows():
+            category4Results.append(str(row['category4']))
+        category4Results = ",".join(category4Results)
 
-    for index, row in single.iterrows():
-      singleResults.append(str(row['single']))
-    singleResults = ",".join(singleResults)
+        for index, row in single.iterrows():
+            singleResults.append(str(row['single']))
+        singleResults = ",".join(singleResults)
 
-    for index, row in groups.iterrows():
-      groupsResults.append(str(row['groups']))
-    groupsResults = ",".join(groupsResults)
+        for index, row in groups.iterrows():
+            groupsResults.append(str(row['groups']))
+        groupsResults = ",".join(groupsResults)
 
-  response = '{"label":[' + labelResults + '],"category1":[' + category1Results + '],"category2":[' + category2Results + '],"category3":[' + \
-      category3Results + '] ,"category4":[' + category4Results + \
-      '],"single":[' + singleResults + '], "groups":[' + groupsResults + ']}'
+    response = '{"label":[' + labelResults + '],"category1":[' + category1Results + '],"category2":[' + category2Results + '],"category3":[' + \
+        category3Results + '] ,"category4":[' + category4Results + \
+        '],"single":[' + singleResults + '], "groups":[' + groupsResults + ']}'
 
-  return response
+    return response
 
 
 @app.route("/static/<path:path>")
